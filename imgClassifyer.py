832425a0e063ad99b82ccb4a920edf47f4dc74b8
hongjinwei <513498646@qq.com>
2014-08-08 10:30:47 +0800

    change the structure
    change the structure

--- imgClassifyer.py
+++ imgClassifyer.py
@@ -1,8 +1,9 @@
-import kNN as kNN
-import img2txt as img2txt
+from lib import kNN as kNN
+from lib import img2txt as img2txt
 
 PATH = './trainingDigits/'
+FILE_PATH = './pic/'
 FILE_NAME = '2.jpg'
-outFileName = img2txt.img2txt(FILE_NAME,32,32)
+outFileName = img2txt.img2txt(FILE_PATH+FILE_NAME,32,32)
 inX = kNN.img2vector(outFileName)
 print kNN.handwritingClassifyer(inX,PATH)
